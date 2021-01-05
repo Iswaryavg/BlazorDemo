13b6361e6def807c0112a1c383f91e42b28b9a71
Iswaryavg <61296893+Iswaryavg@users.noreply.github.com>
2021-01-05 22:08:38 +0000

    Add project files.

---
+++ BlazorDemoApp/Options/EmailSettingOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemoApp.Options
+{
+    public class EmailSettingOptions
+    {
+        public bool EnableEmailSystem { get; set; }
+        public int EmailTimeoutInSeconds { get; set; }
+        public List<string> EmailServer{ get; set; } = new List<string>();
+        public AdminOptions EmailAdmin { get; set; } = new AdminOptions();
+    }
+}
